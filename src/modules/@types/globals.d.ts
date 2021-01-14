47a88b7860282c87e83e3e4140927e3485d4c2b9
S-Del <s-del.pg@protonmail.com>
2021-01-15 05:10:21 +0900

    [fix] NUMBER_EMOJI_LIST の定義と実装を globals.ts に移動

--- src/modules/@types/globals.d.ts
+++ src/modules/@types/globals.d.ts
@@ -1,7 +1,2 @@
 declare type NumberEmoji = ':zero:' | ':one:' | ':two:' | ':three:' | ':four:'
     | ':five:' | ':six:' | ':seven:' | ':eight:' | ':nine:';
-
-declare const NUMBER_EMOJI_LIST: NumberEmoji[] = [
-  ':zero:', ':one:', ':two:', ':three:', ':four:',
-  ':five:', ':six:', ':seven:', ':eight:', ':nine:'
-];
---
+++ src/modules/globals/globals.ts
@@ -0,0 +1,4 @@
+const NUMBER_EMOJI_LIST: readonly NumberEmoji[] = [
+  ':zero:', ':one:', ':two:', ':three:', ':four:',
+  ':five:', ':six:', ':seven:', ':eight:', ':nine:'
+];
