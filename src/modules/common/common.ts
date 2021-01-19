dd43c1fe976159a143d749f53df419dc9997d35f
S-Del <s-del.pg@protonmail.com>
2021-01-19 15:02:12 +0900

    [add] ヘルプ表示機能

--- src/modules/common/common.ts
+++ src/modules/common/common.ts
@@ -6,3 +6,7 @@ export const NUMBER_EMOJI_LIST: readonly NumberEmoji[] = [
 export const UNICODE_NUMBER_EMOJI_LIST: readonly string[] = [
   '0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'
 ];
+
+export const SUB_COMMAND_NAME_LIST: readonly SubCommand[] = [
+  'emoji', 'dice', 'survey', 'help'
+];
