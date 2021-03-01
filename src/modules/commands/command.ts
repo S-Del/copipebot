1873c6dfb1224fb74d304b97e9af2347d59afc14
S-Del <s-del.pg@protonmail.com>
2021-03-02 08:24:26 +0900

    [add] 読み上げ機能

--- src/modules/commands/command.ts
+++ src/modules/commands/command.ts
@@ -1,5 +1,6 @@
 import { Client, Message } from 'discord.js';
 import * as commands from '.';
+import { shouldReadOut } from './speak/speak';
 
 const PREFIX_LIST = ['cb', 'copipebot'] as const;
 const MIN_PREFIX_LEN = PREFIX_LIST.map((prefix) => prefix.length)
@@ -7,7 +8,9 @@ const MIN_PREFIX_LEN = PREFIX_LIST.map((prefix) => prefix.length)
 const MAX_PREFIX_LEN = PREFIX_LIST.map((prefix) => prefix.length)
                                   .reduce((acc, cur) => Math.max(acc, cur));
 
-const COMMAND_LIST = ['emoji', 'dice', 'survey', 'help'] as const;
+const COMMAND_LIST = [
+  'emoji', 'dice', 'survey','join', 'leave', 'help'
+] as const;
 const MIN_COMMAND_NAME_LEN =
     COMMAND_LIST.map((command) => command.length)
                 .reduce((acc, cur) => Math.min(acc, cur));
@@ -25,39 +28,63 @@ const isValidPrefix = (prefix: string): boolean => {
 const isValidCommand = (command: string): boolean => {
   if (command.length < MIN_COMMAND_NAME_LEN) return false;
   if (command.length > MAX_COMMAND_NAME_LEN) return false;
+  if (!COMMAND_LIST.some(item => item == command)) return false;
   return true;
 };
 
+const isCommand = (wordList: string[]): boolean => {
+  return wordList.length >= 2
+         && isValidPrefix(wordList[0])
+         && isValidCommand(wordList[1]);
+};
+
 export const defineCommand = (client: Client): void => {
+  // eslint-disable-next-line complexity
   client.on('message', (message: Message) => {
     if (message.author.bot) return;
+
     const wordList = message.content.split(/\s/);
-    if (wordList.length < 2) return;
-    if (!isValidPrefix(wordList[0])) return;
-
-    const command = wordList[1];
-    if (!isValidCommand(command)) return;
-
-    if (/^emoji$/.test(command)) {
-      commands.emoji(message, wordList.slice(2).join(' '));
+    if (isValidPrefix(wordList[0]) && !isValidCommand(wordList[1])) {
+      void message.reply('コマンドが分かりませんでした');
       return;
     }
 
-    if (/^dice$/.test(command)) {
-      commands.dice(message, wordList[2]);
-      return;
+    if (isCommand(wordList)) {
+      const command = wordList[1];
+      if (/^emoji$/.test(command)) {
+        commands.emoji(message, wordList.slice(2).join(' '));
+        return;
+      }
+
+      if (/^dice$/.test(command)) {
+        commands.dice(message, wordList[2]);
+        return;
+      }
+
+      if (/^survey$/.test(command)) {
+        commands.survey(message, wordList.slice(2));
+        return;
+      }
+
+      if (/^help$/.test(command)) {
+        commands.help(message, COMMAND_LIST);
+        return;
+      }
+
+      if (/^join$/.test(command)) {
+        commands.speak(client, message);
+        return;
+      }
+
+      if (/^leave$/.test(command)) {
+        commands.leave(client, message);
+        return;
+      }
     }
 
-    if (/^survey$/.test(command)) {
-      commands.survey(message, wordList.slice(2));
+    if (shouldReadOut(client, message)) {
+      commands.voicetext(client, message);
       return;
     }
-
-    if (/^help$/.test(command)) {
-      commands.help(message, COMMAND_LIST);
-      return;
-    }
-
-    void message.reply('コマンドが分かりませんでした');
   });
 };
---
+++ src/modules/commands/speak/voicetext.ts
@@ -0,0 +1,69 @@
+import { Client, Message, VoiceConnection } from "discord.js";
+import axios from 'axios';
+import { Readable } from 'stream';
+
+const VOICE_TEXT_API_KEY = process.env.VOICE_TEXT_API_KEY;
+
+const sanitizeText = (text: string): string => {
+  let message = text.replace(/https?:\/\/\S+/g, 'URL省略')
+                    .replace(/<a?:.*?:\d+/g, '')
+                    .replace(/<@&?\d+>/g, '')
+                    .replace(/<#\d+>/g, '');
+  if (message.length > 200) {
+    message = message.slice(0, 196) + ' 以下略';
+  }
+
+  return message;
+};
+
+const readMessage = (
+  message: Message,
+  conn: VoiceConnection
+): Promise<void> => {
+  if (!VOICE_TEXT_API_KEY) {
+    throw new Error('VoiceTextAPI キーが見つかりませんでした ');
+  }
+
+  return axios.request({
+    responseType: 'arraybuffer',
+    url: 'https://api.voicetext.jp/v1/tts',
+    method: 'POST',
+    auth: {
+      username: VOICE_TEXT_API_KEY,
+      password: '',
+    },
+    params: {
+      text: sanitizeText(message.content),
+      speaker: 'show'
+    }
+  }).then(resp => {
+    console.log(`VoiceTextAPI Status: ${resp.status}`);
+    const stream = new Readable();
+    stream.push(resp.data)
+    stream.push(null);
+    conn.play(stream);
+  }).catch(reason => {
+    console.error(reason);
+    void message.reply(
+      'VoiceTextAPI でエラーが発生しています。後ほどお試しください。'
+    );
+    conn.disconnect();
+  });
+};
+
+export const voicetext = (client: Client, message: Message): void => {
+  const guildID = message.guild?.id;
+  if (!guildID) return;
+
+  const conn = client.voice?.connections.get(guildID);
+  if (!conn) return;
+
+  const channel = message.member?.voice.channel;
+  if (!channel) return;
+
+  const botChannelID = conn.channel.id;
+  const memberChannelID = channel.id;
+  if (botChannelID != memberChannelID) return;
+
+  void readMessage(message, conn);
+};
