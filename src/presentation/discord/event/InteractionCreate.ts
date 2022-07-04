1ea6f5f60fc42b5e91afdd3c7858110b4a584590
S-Del <s-del.pg@protonmail.com>
2022-07-05 07:26:36 +0900

    [add] スラッシュコマンド実行時のロギング

--- src/presentation/discord/event/InteractionCreate.ts
+++ src/presentation/discord/event/InteractionCreate.ts
@@ -1,5 +1,5 @@
 import { injectable, multiInject } from 'inversify';
-import { ClientEvents, Interaction } from 'discord.js';
+import { ClientEvents, GuildChannel, Interaction } from 'discord.js';
 import { Symbols } from '../../../config/';
 import { ISlashCommand } from '../slash-command/';
 import { IClientEvent } from './';
@@ -20,6 +20,16 @@ export class InteractionCreate implements IClientEvent {
         if (!command) return;
 
         await command.execute(interaction);
+
+        const user = interaction.user.username;
+        if ( !(interaction.channel instanceof GuildChannel) ) {
+            console.log(`${user} execute /${command.name()}`);
+            return;
+        }
+
+        const guild = interaction.channel.guild.name;
+        const channel = interaction.channel.name;
+        console.log(`${user}@${guild}:${channel} execute /${command.name()}`);
     }
 
     readonly isOnce = (): boolean => InteractionCreate.IS_ONCE;
