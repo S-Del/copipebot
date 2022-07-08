c9987c7b314d1e64a8c667e48a387ac63173aab5
S-Del <s-del.pg@protonmail.com>
2022-07-08 09:13:28 +0900

    [update] 既に別チャンネルに接続中の場合の処理
    横取り防止のため

--- src/presentation/discord/slash-command/JoinCommand.ts
+++ src/presentation/discord/slash-command/JoinCommand.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9';
 import { CommandInteraction, CacheType, Awaitable, GuildMember } from 'discord.js';
+import { ConnectingChannelMap } from '../../../usecase/voice/map/';
 import { JoinChannelUseCase } from '../../../usecase/voice/';
 import { Symbols } from '../../../config/';
 import { ISlashCommand } from './';
@@ -13,6 +14,8 @@ export class JoinCommand implements ISlashCommand {
 
     constructor(
         @inject(Symbols.UseCase.JoinChannel) private readonly joinChannelUseCase: JoinChannelUseCase
+        @inject(Symbols.UseCase.Map.ConnectingChannelMap)
+        private readonly connectingChannelMap: ConnectingChannelMap,
     ) {}
 
     readonly execute = (interaction: CommandInteraction<CacheType>): Awaitable<void> => {
@@ -30,6 +33,13 @@ export class JoinCommand implements ISlashCommand {
             });
         }
 
+        if (!interaction.guild) return;
+        if (this.connectingChannelMap.has(interaction.guild.id)) {
+            return interaction.reply({
+                content: 'ボットが既に別のチャンネルに接続中のため接続できません',
+                ephemeral: true
+            });
+        }
 
         const label = `${voiceChannel.name} (${voiceChannel.id})`;
         if (!voiceChannel.joinable) {
