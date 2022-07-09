7dd8a4bc0f75c5c10fc71d11087d6cb97a70033a
S-Del <s-del.pg@protonmail.com>
2022-07-10 07:21:02 +0900

    [clean] コマンド用のリポジトリを追加
    リファクタリングの為

---
+++ src/infrastructure/api/discord/ApplicationCommandRepository.ts
@@ -0,0 +1,50 @@
+import { inject, injectable } from 'inversify';
+import { Snowflake } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import {
+    APIApplicationCommand,
+    RESTPostAPIApplicationCommandsJSONBody,
+    Routes
+} from 'discord-api-types/v9';
+import { IApplicationCommandRepository } from '../../../domain/model/api/discord/';
+import { Symbols } from '../../../config/';
+
+@injectable()
+export class ApplicationCommandRepository implements IApplicationCommandRepository {
+    constructor(
+        @inject(Symbols.Discord.ApplicationId) private readonly applicationId: Snowflake,
+        @inject(Symbols.Discord.Rest) private readonly rest: REST
+    ) {}
+
+    readonly delete = async (commandId?: string): Promise<void> => {
+        if (!commandId) return this.deleteAll();
+
+        const route = Routes.applicationCommand(this.applicationId, commandId);
+        this.rest.delete(route)
+    }
+
+    readonly deleteAll = async (): Promise<void> => {
+        const route = Routes.applicationCommands(this.applicationId);
+        this.rest.delete(route);
+    }
+
+    readonly get = async (commandId?: string): Promise<APIApplicationCommand[]> => {
+        if (!commandId) return this.getAll();
+
+        const route = Routes.applicationCommand(this.applicationId, commandId);
+        const command = await this.rest.get(route) as APIApplicationCommand;
+        return [command];
+    }
+
+    readonly getAll = async (): Promise<APIApplicationCommand[]> => {
+        const route = Routes.applicationCommands(this.applicationId);
+        return this.rest.get(route) as Promise<APIApplicationCommand[]>;
+    }
+
+    readonly register = async (
+        ...json: RESTPostAPIApplicationCommandsJSONBody[]
+    ): Promise<void> => {
+        const route = Routes.applicationCommands(this.applicationId);
+        this.rest.put(route, { body: json });
+    }
+}
--- src/register-slashcommand.ts
+++ src/register-slashcommand.ts
@@ -1,22 +1,18 @@
-import { Snowflake } from 'discord.js';
-import { Routes } from 'discord-api-types/v9';
-import { REST } from '@discordjs/rest';
 import { container, Symbols } from './config/';
 import { ISlashCommand } from './presentation/discord/slash-command/';
+import { ApplicationCommandRepository } from './infrastructure/api/discord';
 
 (async (): Promise<void> => {
-    const applicationId = container.get<Snowflake>(Symbols.Discord.ApplicationId);
-    const token = container.get<string>(Symbols.Discord.Token);
-
-    const route = Routes.applicationCommands(applicationId);
-    const rest = new REST({ version: '9' }).setToken(token);
+    const repository = container.get<ApplicationCommandRepository>(
+        Symbols.Infrastructure.ApplicationCommandRepository
+    );
     const jsonList = container.getAll<ISlashCommand>(Symbols.Discord.SlashCommands)
                               .map(command => command.toJSON());
 
     try {
         console.log('===== スラッシュコマンドの登録を開始 =====');
         jsonList.forEach(json => console.log(`- ${json.name}`));
-        await rest.put(route, { body: jsonList });
+        await repository.register(...jsonList);
         console.log('===== 登録完了 =====')
     } catch (err) {
         console.error(err);
