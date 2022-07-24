0749994c6da379e36ea857dd7629c9836160ceba
S-Del <s-del.pg@protonmail.com>
2022-07-24 10:27:12 +0900

    [change] 登録済みコマンドを削除してからコマンドを登録
    不要になったコマンドなどが残り続けたりしないようにするため

--- src/setup.ts
+++ src/setup.ts
@@ -8,6 +8,19 @@ import { container, Symbols } from './config/';
     );
 
     try {
+        console.log('===== 登録済みのスラッシュコマンドを取得 =====');
+        const commands = await repository.get();
+        console.log('===== 取得完了 =====');
+
+        if (commands.length > 0) {
+            console.log(`登録済みコマンド数: ${commands.length}`)
+            commands.map(command => console.log(`- ${command.name}`));
+
+            console.log('===== 登録済みのスラッシュコマンドの削除を開始 =====')
+            await repository.delete();
+            console.log('===== 削除完了 =====');
+        }
+
         console.log('===== スラッシュコマンドの登録を開始 =====');
         const jsonList = container.getAll<ISlashCommand>(Symbols.Discord.SlashCommands)
                                   .map(command => command.toJSON());
