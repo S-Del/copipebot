cfd4feac21be867fef728d273477bd308a64b0d4
S-Del <s-del.pg@protonmail.com>
2023-08-04 02:44:32 +0900

    [add] ローカライゼーション用のプロパティ

--- src/presentation/discord/slash-command/CommandDefinition.ts
+++ src/presentation/discord/slash-command/CommandDefinition.ts
@@ -1,6 +1,8 @@
 interface Definition {
     readonly NAME: string;
+    readonly NAME_JP?: string;
     readonly DESCRIPTION: string;
+    readonly DESCRIPTION_JP?: string;
 }
 
 interface OptionDefinition extends Definition {
